413def7077fd81c165c75922cc978a1d034bd271
Falguni2533 <fjain080@gmail.com>
2026-06-25 01:43:13 +0530

    Done with the swiper

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
 
-import 'gameSwiper/css';
+import './gameSwiper.css';
 
 import {EffectCoverflow, Navigation , Autoplay} from 'swiper/modules';
-function GameSwiper() {
+function GameSwiper({games}) {
+
+ const [active, setActive] = useState(false);
+ const handleToggleVideo=()=>{
+  setActive(!active);
+ }
+
   return (
     <Swiper
         effect={'coverflow'}
@@ -22,13 +28,41 @@ function GameSwiper() {
             modifier: 1,
             slideShadows: true,
         }}
-        autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-        }}
+      //  autoplay={{
+      //       delay: 2500,
+      //       disableOnInteraction: false,
+      //   }}
+        navigation={true}
         modules={[EffectCoverflow, Navigation, Autoplay]}
         className="gameSwiper"
     >
+    
+       { games.map(game => (
+          <SwiperSlide key={game._id}>
+            <div className="gameSlider">
+              <img src={game.img} alt="Game Image" />
+              <div className="content">
+                <h2>{game.title}</h2>
+                <p>{game.description}</p>
+                <div className="buttons">
+                  <a href="#" className="orderBtn">Order Now
+                  </a>
+                  <a href="#" className={`playBtn ${active ? 'active' : undefined}`} onclick={handleToggleVideo}>
+
+                    <span className="pause">
+                      <i className="bi bi-pause-fill"></i>
+                    </span>
+                    <span className="play">
+                      <i className="bi bi-play-fill"></i>
+                    </span>
+                  </a>
+                </div>
+              </div>
+              
+              </div>
+          </SwiperSlide>
+        ))}
+
     </Swiper>
   );
 }
