06183e6fead358cf6762a2e3baab9045fc575f4e
Falguni2533 <fjain080@gmail.com>
2026-06-28 03:56:03 +0530

    added gameSlide

--- src/components/GameSwiper.jsx
+++ src/components/GameSwiper.jsx
@@ -1,5 +1,7 @@
 import React, { useState} from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide} from 'swiper/react';
+
+
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-coverflow';
@@ -32,46 +34,21 @@ function GameSwiper({games}) {
       //       delay: 2500,
       //       disableOnInteraction: false,
       //   }}
-        navigation={true}
+      
         modules={[EffectCoverflow, Navigation, Autoplay]}
         className="gameSwiper"
     >
     
        { games.map(game => (
-          <SwiperSlide key={game._id}>
-            <div className="gameSlider">
-              <img src={game.img} alt="Game Image" />
-              <div className={`video ${active ? 'active' : undefined}`}>
-                <iframe
-                  width="520"
-                  height="300"
-                  src={game.trailer}
-                  title={game.title}
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                  allowFullScreen
-                ></iframe>
-               </div>
-              <div className="content">
-                <h2>{game.title}</h2>
-                <p>{game.description}</p>
-                <div className="buttons">
-                  <a href="#" className="orderBtn">Order Now
-                  </a>
-                  <a href="#" className={`playBtn ${active ? 'active' : undefined}`} onClick={handleToggleVideo}>
-
-                    <span className="pause">
-                      <i className="bi bi-pause-fill"></i>
-                    </span>
-                    <span className="play">
-                      <i className="bi bi-play-fill"></i>
-                    </span>
-                  </a>
-                </div>
-              </div>
-              
-              </div>
-          </SwiperSlide>
-        ))}
+        <SwiperSlide key={game._id}>
+          <GameSlide
+            game={game}
+            active={active}
+            toggleVideo={handleToggleVideo}
+          />
+        </SwiperSlide>
+      ))}
+      
 
     </Swiper>
   );
