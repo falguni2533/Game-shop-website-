8204ea7c8dda6943ca738deac4dec1b84c41e2a7
Falguni2533 <fjain080@gmail.com>
2026-06-29 20:30:00 +0530

    added GameCards

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+ import './gameCard.css';
+
+function GameCard({game}) {
+  return (
+    <div className="game-card">
+      
+    </div>
+  )
+}
+
+export default GameCard;
