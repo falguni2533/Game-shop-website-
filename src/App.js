31fca5aeab7f9c3574a91a2bfed603554be4f594
Falguni2533 <fjain080@gmail.com>
2026-07-01 02:27:12 +0530

    CSS Done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import './App.css';
 import Main from './pages/Main';
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return(
--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -5,17 +5,18 @@ import React from 'react'
 function GameCard({game}) {
   return (
     <div className="col-xl-3 col-lg-4 col-md-6">
-      <img src={game.img} alt={game.title} className="img-fluid" />
-      <a href="#">
-        <i className="gameFeauture"></i>
-      </a>
-      <div className="gameType">
+       <div className="gameCard">
+       <img src={game.img} alt={game.title} className="img-fluid" />
+       <a href="#" className="like">
+        <i className="bi bi-heart-fill"></i>
+        </a>
+        <div className="gameFeature">
         <span className="gameType">{game.level}</span>
         <GameRating  rating={game.rating}/>
-      </div>
-      <div className="gameTitle" mt-4 mb-3>{game.title}</div>
-      <div className="gamePrice">
-        {game.discount  !=0 && (
+         </div>
+         <div className="gameTitle mt-4 mb-3">{game.title}</div>
+          <div className="gamePrice">
+         {game.discount  != 0 && (
           <>
           <span className= "discount">
             <i>{game.discount * 100}%</i>
@@ -23,10 +24,17 @@ function GameCard({game}) {
             <span className="prevPrice">${game.price.toFixed(2)}</span>
             </>
         )}
-       </div>
+        <span className="currectPrice">
+          ${(( 1 - game.discount) * game.price).toFixed(2)}
+        </span>
+        </div>
+        <a href="#" className="addbag">
+        <i className="bi bi-bag-plus-fill"></i>
+        </a>
+      </div>
     </div>
     
   );
 }
-
+ 
 export default GameCard;
