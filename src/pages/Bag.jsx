84a0ad230ac84407b5c43d62ce5d773df5c983b3
falguni2533 <fjain080@gmail.com>
2026-07-03 04:07:29 +0530

    navigation

---
+++ src/pages/Bag.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Bag.css'
+
+function Bag() {
+  return  (
+  <section id="bag" className="bag">
+    <h1>My Bag</h1>
+  </section>
+  )
+    
+}
+
+export default Bag
